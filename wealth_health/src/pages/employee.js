367dc257f9e4d1e8471f5bf3d2b003e7992f7758
swanndolia <swanndolia@gmail.com>
2023-05-05 19:48:12 +0200

    remove old code from github

--- wealth_health/src/pages/employee.js
+++ wealth_health/src/pages/employee.js
@@ -1,7 +1,6 @@
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import Table from '../components/table';
-
+import Table from '../components/table'
 const Employee = () => {
     const employees = useSelector((state) => state.users.list);
     const tableHead = ["First Name", "Last Name", "Start Date", "Date of Birth", "Department", "Street", "City", "State", "Zip Code"]
